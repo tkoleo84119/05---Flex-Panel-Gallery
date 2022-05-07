656f61b6ad3bc2f7b73a69499035b6fb130cd9ed
Kerwin <tkoleo84119@gmail.com>
2022-05-08 00:33:05 +0800

    feat: add js setting

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const panels = document.querySelector(".panels");
 
+panels.addEventListener("click", openPanel);
+panels.addEventListener("transitionend", moveWord);
+
+function openPanel(e) {
+  const panel = e.target.closest(".panel");
+  panel.classList.toggle("open");
+}
+
+function moveWord(e) {
+  Array.from(e.target.children).map((p) => {
+    if (e.target.classList.contains("open")) {
+      p.classList.add("move-word");
+    } else {
+      p.classList.remove("move-word");
+    }
+  });
+}
