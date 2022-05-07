b80e8a2c06d07d92414f938922d6b059cc3921ad
Kerwin <tkoleo84119@gmail.com>
2022-05-08 00:46:07 +0800

    feat: add Js Doc

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const panels = document.querySelector(".panels");
 panels.addEventListener("click", openPanel);
 panels.addEventListener("transitionend", moveWord);
 
+/**
+ * This function is add/remove "open" class to the panel which be click
+ * @param {object} e => event object
+ */
 function openPanel(e) {
   const panel = e.target.closest(".panel");
   panel.classList.toggle("open");
 }
 
+/**
+ * This function is add/remove "move-word" class to the p element which be click
+ * @param {object} e => event object
+ */
 function moveWord(e) {
   Array.from(e.target.children).map((p) => {
     if (e.target.classList.contains("open")) {
